340e0331de36ab0f83e162e5b01bd5b8beab835f
ominusliticus <elkevin111@gmail.com>
2024-02-07 17:14:21 -0600

    Updated ccake scripts to reflect new EoS and EoMs

--- plots/for_ccake/generate_initial_conditions.py
+++ plots/for_ccake/generate_initial_conditions.py
@@ -32,7 +32,13 @@ class Config:
         self.tau_f: Optional[float] = None
         self.tau_step: Optional[float] = None
         self.temp_0: Optional[float] = None
-        self.mu_0: Optional[float] = None
+        self.muB_0: Optional[float] = None
+        self.muS_0: Optional[float] = None
+        self.muQ_0: Optional[float] = None
+        self.ceos_temp_0: Optional[float] = None
+        self.ceos_muB_0: Optional[float] = None
+        self.ceos_muS_0: Optional[float] = None
+        self.ceos_muQ_0: Optional[float] = None
         self.pi_0: Optional[float] = None
         self.tol: Optional[float] = None
         self.output_dir: Optional[str] = None
@@ -43,21 +49,41 @@ class Config:
         with open('run.cfg', 'r') as f:
             lines = f.readlines()
             for line in lines:
+                # Initialization stuff
                 key, value = line.split()[:2]
+                print(key, value)
                 if key == 'tau_0':
                     self.tau_0 = float(value)
                 elif key == 'tau_f':
                     self.tau_f = float(value)
                 elif key == 'tau_step':
-                    self.tau_step == float(value)
+                    self.tau_step = float(value)
                 elif key == 'temp_0':
                     self.temp_0 = float(value)
-                elif key == 'mu_0':
-                    self.mu_0 = float(value)
-                    if self.mu_0 == 0:
-                        self.mu_0 = 1e-20
+                elif key == 'muB_0':
+                    self.muB_0 = float(value)
+                    if self.muB_0 == 0:
+                        self.muB_0 = 1e-20
+                elif key == 'muS_0':
+                    self.muS_0 = float(value)
+                    if self.muS_0 == 0:
+                        self.muS_0 = 1e-20
+                elif key == 'muQ_0':
+                    self.muQ_0 = float(value)
+                    if self.muQ_0 == 0:
+                        self.muQ_0 = 1e-20
                 elif key == 'pi_0':
                     self.pi_0 = float(value)
+                # EOS stuff
+                elif key == 'ceos_temp_0':
+                    self.ceos_temp_0 = float(value)
+                elif key == 'ceos_muB_0':
+                    self.ceos_muB_0 = float(value)
+                elif key == 'ceos_muS_0':
+                    self.ceos_muS_0 = float(value)
+                elif key == 'ceos_muQ_0':
+                    self.ceos_muQ_0 = float(value)
+                # Utility
                 elif key == 'tolerance':
                     self.tol = float(value)
                 elif key == 'output_dir':
@@ -70,21 +96,28 @@ if __name__ == "__main__":
     # Here we have to convert to the corresponding dimensionless variables
     #   for de Sitter space
     temp_0 = cfg.temp_0 * cfg.tau_0 / HBARC
-    mu_0 = cfg.mu_0 * cfg.tau_0 / HBARC
-    y0s = array([temp_0, mu_0, cfg.pi_0])
+    muB_0 = cfg.muB_0 * cfg.tau_0 / HBARC
+    muS_0 = cfg.muS_0 * cfg.tau_0 / HBARC
+    muQ_0 = cfg.muQ_0 * cfg.tau_0 / HBARC
+    y0s = array([temp_0, muB_0, muS_0, muQ_0, cfg.pi_0])
+
+    ceos_temp_0 = cfg.ceos_temp_0
+    ceos_mu_0 = array([cfg.ceos_muB_0, cfg.ceos_muS_0, cfg.ceos_muQ_0])
+    consts = {'temperature_0': ceos_temp_0, 'chem_potential_0': ceos_mu_0}
 
     rhos_1 = linspace(-10, 0, 1000)[::-1]
     rhos_2 = linspace(0, 10, 1000)
 
-    soln_1 = odeint(eom, y0s, rhos_1)
-    soln_2 = odeint(eom, y0s, rhos_2)
+    soln_1 = odeint(eom, y0s, rhos_1, args=(ceos_temp_0, ceos_mu_0,))
+    soln_2 = odeint(eom, y0s, rhos_2, args=(ceos_temp_0, ceos_mu_0,))
     t_hat = concatenate((soln_1[:, 0][::-1], soln_2[:, 0]))
-    mu_hat = concatenate((soln_1[:, 1][::-1], soln_2[:, 1]))
-    pi_bar_hat = concatenate((soln_1[:, 2][::-1], soln_2[:, 2]))
+    mu_hat = [concatenate((soln_1[:, i][::-1], soln_2[:, i]))
+              for i in [1, 2, 3]]
+    pi_bar_hat = concatenate((soln_1[:, 4][::-1], soln_2[:, 4]))
     rhos = concatenate((rhos_1[::-1], rhos_2))
 
     t_interp = interp1d(rhos, t_hat)
-    mu_interp = interp1d(rhos, mu_hat)
+    mu_interp = [interp1d(rhos, f) for f in mu_hat]
     pi_interp = interp1d(rhos, pi_bar_hat)
 
     tau = cfg.tau_0
@@ -99,7 +132,7 @@ if __name__ == "__main__":
     hbarc = 0.1973269804 
 
     # Write header
-    dir_name = f'tau0={cfg.tau_0:.2f}_T0={cfg.temp_0:.2f}_mu0={cfg.mu_0:.2f}_pi0={cfg.pi_0:.2f}'
+    dir_name = f'tau0={cfg.tau_0:.2f}_T0={cfg.temp_0:.2f}_muB0={cfg.muB_0:.2f}__muS0={cfg.muS_0:.2f}_muQ0={cfg.muQ_0:.2f}pi0={cfg.pi_0:.2f}'
     dir_path = Path(dir_name).absolute()
 
     try:
@@ -114,16 +147,42 @@ if __name__ == "__main__":
 
         for x in arange(xmin, xmax, stepx):
             for y in arange(ymin, ymax, stepy):
-                pis = milne_pi(tau, x, y, 1.0, t_interp, mu_interp, pi_interp, cfg.tol)
+                pis = milne_pi(
+                    tau=tau,
+                    x=x, 
+                    y=y, 
+                    q=1.0, 
+                    ads_T=t_interp, 
+                    ads_mu=mu_interp,
+                    ads_pi_bar_hat=pi_interp,
+                    **consts,
+                    tol=cfg.tol
+                )
                 f.write('{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}\n'
                         .format(
                             x,
                             y,
                             0,  # eta
-                            milne_energy(tau, x, y, 1.0, t_interp, mu_interp, cfg.tol),
-                            milne_number(tau, x, y, 1.0, t_interp, mu_interp, cfg.tol),
-                            0,  # rho_S
-                            0,  # rho_Q
+                            milne_energy(
+                                tau=tau,
+                                x=x,
+                                y=y,
+                                q=1.0,
+                                ads_T=t_interp,
+                                ads_mu=mu_interp,
+                                **consts,
+                                tol=cfg.tol
+                            ),
+                            *milne_number(
+                                tau=tau,
+                                x=x,
+                                y=y,
+                                q=1.0,
+                                ads_T=t_interp,
+                                ads_mu=mu_interp,
+                                **consts,
+                                tol=cfg.tol
+                            ),
                             u_x(tau, x, y, 1.0),
                             u_y(tau, x, y, 1.0),
                             0,  # u_eta
